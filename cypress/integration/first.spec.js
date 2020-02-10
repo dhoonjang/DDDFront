316336ae379d7c2a3e84a59be8ac35b6a017c823
dhoonjang <dhoonjang@gmail.com>
2020-02-10 19:41:39 +0900

    fix: cypress & gitlab ci bug

--- cypress/integration/first.spec.js
+++ cypress/integration/first.spec.js
@@ -4,6 +4,13 @@ describe("My First Test", function() {
 
     cy.get(".iddakdae-logo").click();
     cy.get(".ikakao-login-btn").click();
+
+    cy.get('label[for="id_email_2"]').click();
+    cy.get("#id_email_2").type("dhoonjang@gmail.com");
+    cy.get('label[for="id_password_3"]').click();
+    cy.get("#id_password_3").type("wkdgkgk123");
+
+    cy.get(".wrap_btn > button.btn_confirm.submit").click();
   });
 
   it("Join Page Action", function() {
